9d503e8c95d2e18fc3813f5f6c396d4dde2b3886
tomspalding <tspalding@effectual.com>
2019-06-20 18:14:47 -0400

    not saving this

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,70 +1,43 @@
-import React, { Component } from "react";
-import axios from "axios";
-class Contact extends Component {
-  state = {
-    name: "",
-    message: "",
-    email: "",
-    sent: false,
-    buttonText: "Send Message"
-  };
-  render() {
-    return (
-      <div className="container">
-        <h1>Get in touch!</h1>
-        <form
-          id="contact-form"
-          onSubmit={this.handleSubmit.bind(this)}
-          method="POST"
-        >
-          <div className="form-group">
-            <label for="name">Name</label>
-            <input type="text" className="form-control" id="name" />
-          </div>
-          <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-            />
-          </div>
-          <div className="form-group">
-            <label for="message">Message</label>
-            <textarea className="form-control" rows="5" id="message" />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    axios({
-      method: "POST",
-      url: "http://localhost:3002/send",
-      data: {
-        name: name,
-        email: email,
-        messsage: message
-      }
-    }).then(response => {
-      if (response.data.msg === "success") {
-        alert("Message Sent.");
-        this.resetForm();
-      } else if (response.data.msg === "fail") {
-        alert("Message failed to send.");
-      }
-    });
-  }
-  resetForm() {
-    document.getElementById("contact-form").reset();
-  }
-}
-export default Contact;
+import React from "react";
+
+const ContactForm = () => (
+  <form className="contact-form" method="POST" action="/contact">
+    <div className="form-field">
+      <label htmlFor="name">
+        <div className="label-content">Name:</div>
+        <input type="text" name="name" required />
+      </label>
+    </div>
+
+    <div className="form-field">
+      <label htmlFor="email">
+        <div className="label-content">Email:</div>
+        <input type="email" name="email" required />
+      </label>
+    </div>
+
+    <div className="form-field">
+      <label htmlFor="message">
+        <div className="label-content">Message:</div>
+        <textarea className="stretch" name="message" rows="5" required />
+      </label>
+    </div>
+
+    <button type="submit">Send</button>
+
+    <div>
+      {window.location.hash === "#success" && (
+        <div id="success">
+          <p>Your message has been sent!</p>
+        </div>
+      )}
+      {window.location.hash === "#error" && (
+        <div id="error">
+          <p>An error occured while submitting the form.</p>
+        </div>
+      )}
+    </div>
+  </form>
+);
+
+export default ContactForm;
