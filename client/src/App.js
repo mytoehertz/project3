f1dd20d9afec6eb5b2c38381b43cbad0f3ce3114
tomspalding <tspalding@effectual.com>
2019-06-20 20:54:10 -0400

    starting the footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import React, { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Routes from './components/routing/Routes';
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Routes from "./components/routing/Routes";
 import { ProductProvider } from "../src/utils/context";
 
 // Redux
-import { Provider } from 'react-redux';
-import store from './store';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
-import './App.css';
+import "./App.css";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -24,16 +25,17 @@ const App = () => {
 
   return (
     <Provider store={store}>
-    <ProductProvider>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route component={Routes} />
-          </Switch>
-        </Fragment>
-      </Router>
+      <ProductProvider>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Landing} />
+              <Route component={Routes} />
+            </Switch>
+            <Footer />
+          </Fragment>
+        </Router>
       </ProductProvider>
     </Provider>
   );
