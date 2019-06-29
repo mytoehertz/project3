3a1ab659c1a3f4078bfb93c676bc6f010abe270a
Tasha <natalytak@yahoo.com>
2019-06-29 09:50:46 -0400

    Fixing heroku pathing.

--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -2,7 +2,7 @@ export const storeProducts = [
     {
         id: 1,
         name:"Zebra Plant",
-        img: "client/public/img/zebra_plant.jpg",
+        img: "img/zebra_plant.jpg",
         price: 80,
         size: "XS",
         inCart: false,
@@ -17,7 +17,7 @@ export const storeProducts = [
     {
         id: 2,
         name: "Assorted Succulent",
-        img: "client/public/img/succulent.jpg",
+        img: "img/succulent.jpg",
         price: 95,
         size: "XS",
         inCart: false,
@@ -32,7 +32,7 @@ export const storeProducts = [
     {
         id: 3,
         name: "Assorted Cactus",
-        img: "client/public/img/cactus.jpg",
+        img: "img/cactus.jpg",
         price: 35,
         size: "S",
         inCart: false,
@@ -47,7 +47,7 @@ export const storeProducts = [
     {
         id: 4,
         name: "Chinese Money Plant",
-        img: "client/public/img/chinese_money.jpg",
+        img: "img/chinese_money.jpg",
         price: 95,
         size: "S",
         inCart: false,
@@ -62,7 +62,7 @@ export const storeProducts = [
     {
         id: 5,
         name: "Bella Palm",
-        img: "client/public/img/parlor_palm.jpg",
+        img: "img/parlor_palm.jpg",
         price: 105,
         size: "S",
         inCart: false,
@@ -77,7 +77,7 @@ export const storeProducts = [
     {
         id: 6,
         name:"Spider Plant",
-        img: "client/public/img/spider_plant.jpg",
+        img: "img/spider_plant.jpg",
         price: 60,
         size: "S",
         inCart: false,
@@ -92,7 +92,7 @@ export const storeProducts = [
     {
         id: 7,
         name:"Pears and Jade",
-        img: "client/public/img/pearls_and_jade.jpg",
+        img: "img/pearls_and_jade.jpg",
         price: 55,
         size: "S",
         inCart: false,
@@ -107,7 +107,7 @@ export const storeProducts = [
 {
         id: 8,
         name:"Stromanthe",
-        img: "client/public/img/stromanthe.jpg",
+        img: "img/stromanthe.jpg",
         price: 60,
         size: "MD",
         inCart: false,
@@ -122,7 +122,7 @@ export const storeProducts = [
 {
     id: 9,
     name:"Marble Queen",
-    img: "client/public/img/marble_queen.jpg",
+    img: "img/marble_queen.jpg",
     price: 70,
     size: "SM",
     inCart: false,
@@ -137,7 +137,7 @@ export const storeProducts = [
 {
     id: 10,
     name:"Air Plant",
-    img: "client/public/img/air_plant.jpg",
+    img: "img/air_plant.jpg",
     price: 30,
     size: "XS",
     inCart: false,
@@ -152,7 +152,7 @@ export const storeProducts = [
 {
     id: 11,
     name:"Calathea",
-    img: "client/public/img/calathea.jpg",
+    img: "img/calathea.jpg",
     price: 55,
     size: "SM",
     inCart: false,
@@ -167,7 +167,7 @@ export const storeProducts = [
 {
     id: 12,
     name:"Alocasia",
-    img: "client/public/img/alocasia.jpg",
+    img: "img/alocasia.jpg",
     price: 55,
     size: "MD",
     inCart: false,
@@ -184,7 +184,7 @@ export const storeProducts = [
 export const detailProduct = {
     id:"5d0f72aa0f66b75f1b55960e",
     name:"Zebra Plant",
-    img: "client/public/img/zebra_plant.jpg",
+    img: "img/zebra_plant.jpg",
     price: 80,
     size: "xs",
     inCart: false,
